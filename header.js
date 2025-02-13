a3e1daf6d12e727d0db79d326907e22c9a299c56
schw4b <schw4b@gmail.com>
2025-02-13 13:42:46 +0100

    added analytic tracker

---
+++ header.js
@@ -0,0 +1,12 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4Q01TBQHST"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4Q01TBQHST');
+</script>
+
+<!-- favicon -->
+<link rel="icon" type="image/png" href="./images/Kreis_Niere_196x196.png">
